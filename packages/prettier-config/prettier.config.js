ca76ee5293eadda66b59ac4b77579c19fc9a7be5
Andrew Wang <andrew@andrew0.com>
2026-04-20 11:37:25 -0700

    Initial commit

---
+++ packages/prettier-config/prettier.config.js
@@ -0,0 +1,21 @@
+import { createRequire } from "node:module";
+
+const require = createRequire(import.meta.url);
+
+const config = {
+  endOfLine: "lf",
+  tabWidth: 2,
+  printWidth: 100,
+  plugins: [
+    require.resolve("prettier-plugin-embed"),
+    require.resolve("prettier-plugin-packagejson"),
+    require.resolve("prettier-plugin-sh"),
+    require.resolve("prettier-plugin-sql"),
+    require.resolve("prettier-plugin-tailwindcss"),
+  ],
+
+  // tailwindcss plugin
+  tailwindFunctions: ["cva", "clsx", "cn"],
+};
+
+export default config;
